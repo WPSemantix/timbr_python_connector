d262c3875008a05b09ce04554b85097f901cf609
Dan W <41392410+semantiDan@users.noreply.github.com>
2022-08-22 15:02:04 +0300

    removed sneaky extra blank line

--- timbr_connector.py
+++ timbr_connector.py
@@ -34,6 +34,5 @@ def get_combined_jars_path(maindir):
 jars_path = get_combined_jars_path(main_jar_path)
 
 def getConnection(jdbc_url, username, password):
-
     conn = jaydebeapi.connect(jdbc_driver, jdbc_url, [username, password], jars_path)
     return conn
